344a9ef4b2b951db82605aa2a0f1629371d53b02
Alexandr Bespalov <ya.aleksandr-i2013@yandex.ru>
2024-02-25 18:03:03 +0300

    vse good

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCards (item) {
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    placesItem.querySelector('.card__image').src = item.link;
+    placesItem.querySelector('.card__title').textContent = item.name;
+
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCards);
+
+    return placesItem;
+};
 
 // @todo: Функция удаления карточки
+function deleteCards (event) {
+    deleteButton = event.target.closest('.places__item');
+    deleteButton.remove()
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach (function (item, placesItem) {
+    placesItem = addCards(item);
+    placesList.append(placesItem);
+});
